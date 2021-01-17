40bbc8187e6fae7cacbce7c032f3792c38faf3e2
harismuntazir <haris.0813.m@gmail.com>
2021-01-17 10:34:26 +0530

    Create create a dialog box using kotlin.kt

---
+++ create a dialog box using kotlin.kt
@@ -0,0 +1,13 @@
+val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Dialog Title Here")
+            setPositiveButton("OK") { _, _ ->
+                //do positive action
+            }
+            setNegativeButton("Cancel") { _, _ ->
+                //do negative action
+            }
+            builder.create().show()
+        }
